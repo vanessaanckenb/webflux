e44c5662fd01b29f804a03bbf4e24c0068dc3b3a
Vanessa Fernanda Costa Won Ancken Barretos <vanessa.ancken@sennatech.com.br>
2023-09-08 09:39:38 -0300

    usercontoller interface without models

---
+++ src/main/java/br/com/vancken/webflux/controller/UserController.java
@@ -0,0 +1,24 @@
+package br.com.vancken.webflux.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface UserController {
+
+    @PostMapping
+    ResponseEntity<Mono<UserResponse>> save(@RequestBody UserRequest request);
+
+    @GetMapping("/{id}")
+    ResponseEntity<Mono<UserResponse>> find(@PathVariable String id);
+
+    @GetMapping
+    ResponseEntity<Flux<UserResponse>> findAll();
+
+    @PatchMapping("/{id}")
+    ResponseEntity<Mono<UserResponse>> update(@PathVariable String id, @RequestBody UserRequest request);
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<Mono<Void>> delete(@PathVariable String id);
+}
